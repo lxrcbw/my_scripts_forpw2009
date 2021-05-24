cf021296caecc33c50a001142d0e4c3d8fd10fbe
lpssxs <731588359@qq.com>
2021-05-24 18:20:17 +0800

    git fix

--- shell/jd/lpss_git.sh
+++ shell/jd/lpss_git.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 # 脚本作者 ：  lpssxs
-# 更新时间 ：  2021/5/13
+# 更新时间 ：  2021/5/24
 
-cd  /root/jd/scripts
+rm -rf /ql/repo/lpssxs/*.js
+cp -rf /ql/repo/chinnkarahoi_jd_scripts/jd*.js /ql/repo/lpssxs/
+find /ql/repo/monk-coder_dust/ -name "monk_*.js"  -exec cp -rf {} /ql/repo/lpssxs/ \;
+find /ql/repo/monk-coder_dust/ -name "adolf_*.js"  -exec cp -rf {} /ql/repo/lpssxs/ \;
+find /ql/repo/nianyuguai_longzhuzhu/ -name "jd_*.js"  -exec cp -rf {} /ql/repo/lpssxs/ \;
+find /ql/repo/whyour_hundun/ -name "jx_*.js"  -exec cp -rf {} /ql/repo/lpssxs/ \;
+
+cd  /ql/repo/lpssxs
 
 git add .
 
 remark=$(date +"%Y-%m-%d %H:%M:%S")
-git commit -m "更新时间：${remark}"
+git commit -m "同步时间：${remark}"
 
-git pull origin master
-git push origin master
+git pull origin main
+git push origin main
 
 echo "JD Scripts 同步成功~"
