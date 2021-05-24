811f9f0576816653f02a9121a00336158775da24
lpssxs <731588359@qq.com>
2021-05-24 21:23:36 +0800

    send666

--- shell/jd/lpss_git.sh
+++ shell/jd/lpss_git.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # 脚本作者 ：  lpssxs
 # 更新时间 ：  2021/5/24
@@ -7,6 +7,7 @@
 cp -rf /ql/repo/chinnkarahoi_jd_scripts/jd*.js /ql/repo/lpssxs/lxk/
 find /ql/repo/monk-coder_dust/ -name "monk_*.js"  -exec cp -rf {} /ql/repo/lpssxs/monk/ \;
 find /ql/repo/monk-coder_dust/ -name "adolf_*.js"  -exec cp -rf {} /ql/repo/lpssxs/adolf/ \;
+find /ql/repo/monk-coder_dust/ -name "z_*.js"  -exec cp -rf {} /ql/repo/lpssxs/chenzhe/ \;
 find /ql/repo/nianyuguai_longzhuzhu/ -name "jd_*.js"  -exec cp -rf {} /ql/repo/lpssxs/longzhuzhu/ \;
 find /ql/repo/whyour_hundun/ -name "jx_*.js"  -exec cp -rf {} /ql/repo/lpssxs/whyour/ \;
 find /ql/repo/whyour_hundun/ -name "jd*.js"  -exec cp -rf {} /ql/repo/lpssxs/whyour/ \;
@@ -21,4 +22,4 @@ git commit -m "同步时间：${remark}"
 git pull origin main --allow-unrelated-histories
 git push origin main
 
-echo "JD Scripts 同步成功~"
+echo "${remark} JD Scripts 同步成功~"
