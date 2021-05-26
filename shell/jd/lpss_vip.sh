a017b0ba652dae15bf1243e72944738dabd13599
lpssxs <731588359@qq.com>
2021-05-26 22:04:12 +0800

    vip2

--- shell/jd/lpss_vip.sh
+++ shell/jd/lpss_vip.sh
@@ -55,7 +55,6 @@ fi
 # 下载需要添加的脚本 link 
 
 wget -q --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/monk-coder/dust/dust/normal/monk_shop_lottery.js -O /jd/scripts/jd_monk_shop_lottery.js
-wget -q --no-check-certificate https://jdsharedresourcescdn.azureedge.net/jdresource/jd_syj.js -O /jd/scripts/jd_syj.js 
 wget -q --no-check-certificate https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_super_redrain.js  -O /jd/scripts/jd_npc_redrain.js 
 wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/js_code/jd/jd_daily_lottery.js -O /jd/scripts/jd_daily_lottery.js
 wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/js_code/jd/jd_redPacket.js -O /jd/scripts/jd_redPacket.js
@@ -89,7 +88,6 @@ lpss_vip
 jd_super_redrain
 jd_half_redrain
 jd_clear_shop
-jd_syj
 jd_npc_redrain
 jd_daily_lottery
 jd_monk_shop_lottery
@@ -112,7 +110,6 @@ jd_super_redrain="0 0-23/1 * * * jd jd_super_redrain"
 jd_half_redrain="30 20-23/1 * * * jd jd_half_redrain"
 jd_clear_shop="45 22 * * * jd jd_clear_shop"
 jd_monk_shop_lottery="1 0,10,23 * * * jd jd_monk_shop_lottery"
-jd_syj="10 0,7,23 * * * jd jd_syj"
 jd_npc_redrain="0 0-23/1 * * * jd jd_npc_redrain"
 jd_daily_lottery="13 1,22,23 * * * jd jd_daily_lottery"
 jd_adolf_pk="15 8,13,18 17-31 5 * jd jd_adolf_pk"
