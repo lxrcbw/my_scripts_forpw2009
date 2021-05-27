07bcb05186b8e83953e7c33112bcff1e4536d294
lpssxs <731588359@qq.com>
2021-05-27 09:37:44 +0800

    jd_npc_zoo

--- shell/jd/lpss_diy.sh
+++ shell/jd/lpss_diy.sh
@@ -92,6 +92,7 @@ wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_sc
 wget -q --no-check-certificate https://raw.githubusercontent.com/monk-coder/dust/dust/normal/adolf_superbox.js -O /jd/scripts/jd_adolf_superbox.js
 wget -q --no-check-certificate https://raw.githubusercontent.com/monk-coder/dust/dust/normal/adolf_newInteraction.js -O /jd/scripts/jd_adolf_newInteraction.js
 wget -q --no-check-certificate https://raw.githubusercontent.com/monk-coder/dust/dust/normal/adolf_jxhb.js -O /jd/scripts/jd_adolf_jxhb.js
+wget -q --no-check-certificate https://raw.githubusercontent.com/yangtingxiao/QuantumultX/master/scripts/jd/jd_zoo.js -O /jd/scripts/jd_npc_zoo.js
 
 #添加需要添加的脚本 name
 my_scripts_list_add="
@@ -112,6 +113,7 @@ jd_jump
 jd_adolf_urge
 jd_z_carnivalcity
 jd_lxk_zoo
+jd_npc_zoo
 jd_lxk_city
 jd_adolf_superbox
 jd_adolf_newInteraction
@@ -144,6 +146,7 @@ jd_jay_freshgoods="22 22 * * * jd jd_jay_freshgoods"
 jd_adolf_superbox="11 9,20 * 5,6 * jd jd_adolf_superbox"
 jd_adolf_newInteraction="15 9,20 * 5,6 * jd jd_adolf_newInteraction"
 jd_adolf_jxhb="25 9 * 5,6 * jd jd_adolf_jxhb"
+jd_npc_zoo="34 * * * *  jd jd_npc_zoo"
 
 
 
