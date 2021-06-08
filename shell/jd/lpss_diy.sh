eb9854e9cf8b02bd4b1901e3a71eabfb1357a6fb
lpssxs <731588359@qq.com>
2021-06-08 08:37:19 +0800

    del gh2

--- shell/jd/lpss_diy.sh
+++ shell/jd/lpss_diy.sh
@@ -42,8 +42,8 @@ fi
 if [ $my_download_flag = "opens" ];then
 
 	echo -e "开始更新 home.html & sendNotify.js 文件\n"
-	wget -q --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/forpw2009/my_scripts/main/html/home.html  -O /jd/home.html
-	wget -q --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/forpw2009/my_scripts/main/js_code/sendNotify_FLC.js -O /jd/sendNotify.js 
+	wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/html/home.html  -O /jd/home.html
+	wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/js_code/sendNotify_FLC.js -O /jd/sendNotify.js 
 	wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/shell/jd/lpss_lxk.sh -O /jd/lpss_lxk.sh && chmod +x /jd/lpss_lxk.sh
 	wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/shell/jd/lpss_del_log.sh -O /jd/config/lpss_del_log.sh  && chmod +x /jd/config/lpss_del_log.sh
     wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/shell/jd/export_sharecodes.sh -O /jd/export_sharecodes.sh && chmod +x /jd/export_sharecodes.sh
