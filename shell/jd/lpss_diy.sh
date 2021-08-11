c35b0548f84c340dac7f1335fb18a67b5fb0c54c
lpssxs <731588359@qq.com>
2021-08-11 10:28:56 +0800

    Update lpss_diy.sh

--- shell/jd/lpss_diy.sh
+++ shell/jd/lpss_diy.sh
@@ -129,7 +129,8 @@ wget -q --no-check-certificate https://raw.githubusercontent.com/shufflewzc/fake
 wget -q --no-check-certificate https://raw.githubusercontent.com/shufflewzc/faker2/main/jx_sign.js -O /jd/scripts/jx_sign.js
 wget -q --no-check-certificate https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_nzmh.js -O /jd/scripts/jd_nzmh.js
 wget -q --no-check-certificate https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_ryhxj.js -O /jd/scripts/jd_ryhxj.js
-wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/backup/main/smiek2221/gua_opencard8.js -O /jd/scripts/gua_opencard8.js
+wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/backup/main/smiek2221/gua_opencard9.js -O /jd/scripts/gua_opencard9.js
+wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/backup/main/smiek2221/gua_opencard10.js -O /jd/scripts/gua_opencard10.js
 wget -q --no-check-certificate https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_ylyn.js -O /jd/scripts/jd_ylyn.js
 
 
@@ -148,7 +149,8 @@ jd_ldhwj
 jx_sign
 jd_nzmh
 jd_ryhxj
-gua_opencard8
+gua_opencard9
+gua_opencard10
 jd_ylyn
 "
 
@@ -222,13 +224,13 @@ jd_ldhwj="11 1,9,15,20 * * * jd jd_ldhwj"
 jd_nzmh="35 1,8,15,23 * * * jd jd_nzmh"
 jx_sign="20 1,8,15,22 * * * jd jx_sign"
 jd_ryhxj="1 1 * * * jd jd_ryhxj"
-gua_opencard8="3,45 0,23 * * * jd gua_opencard8"
+gua_opencard9="45 11 * * * jd gua_opencard9"
+gua_opencard10="45 11 * * * jd gua_opencard10"
 jd_ylyn="40 5,18 * * * jd jd_ylyn"
 
 #添加需要删除的脚本的 name
 my_scripts_list_del="
-jd_star_movement_bet
-jd_star_movement_map
+gua_opencard8
 "
 
 #awk '{print $NF}' filename
