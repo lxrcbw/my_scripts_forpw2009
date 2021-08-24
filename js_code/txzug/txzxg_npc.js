4d858735071fe4adf6e189101c8cc51b8814e83a
lpssxs <731588359@qq.com>
2021-08-24 16:35:40 +0800

    Update txzxg_npc.js

--- js_code/txzug/txzxg_npc.js
+++ js_code/txzug/txzxg_npc.js
@@ -80,36 +80,36 @@ if ($.isNode()) {
 }
 
 let userheaderArr = [];
-let userheaderVal = "oA0GbjhRW4-KztUg8E8SpN7z7kB0&_appName=ios&_dev=iPhone10,6&_devId=8661a562dacc175efc5a8ee552a4dcfa4a31fc53&_appver=8.7.1&_ifChId=&_isChId=1&_osVer=14.3&openid=oA0GbjhRW4-KztUg8E8SpN7z7kB0&fskey=v0aaf63c220600b82a27e95fb57bb1ff&appid=wxcbc3ab3807acb685&access_token=41_rlGfPc3HxQeXrVO9Jbv9xTvKftAP6ZZu_Hrohp81i9bCu1IU0MjlI6ieqKQ7yeT_5V94S3Cg0FAppBcBQU4G_v_LDQvUINSFHEbGvHOufJY&buildType=store&check=11&_idfa=E67F03D3-A70A-4280-8289-802EE71D155C&lang=zh_CN";
-let USERHEADER = [];
+let userheaderVal = "";
+let USERHEADER = 'oA0GbjhRW4-KztUg8E8SpN7z7kB0&_appName=ios&_dev=iPhone10,6&_devId=8661a562dacc175efc5a8ee552a4dcfa4a31fc53&_appver=8.7.1&_ifChId=&_isChId=1&_osVer=14.3&openid=oA0GbjhRW4-KztUg8E8SpN7z7kB0&fskey=v0aaf63c220600b82a27e95fb57bb1ff&appid=wxcbc3ab3807acb685&access_token=41_rlGfPc3HxQeXrVO9Jbv9xTvKftAP6ZZu_Hrohp81i9bCu1IU0MjlI6ieqKQ7yeT_5V94S3Cg0FAppBcBQU4G_v_LDQvUINSFHEbGvHOufJY&buildType=store&check=11&_idfa=E67F03D3-A70A-4280-8289-802EE71D155C&lang=zh_CN';
 
 let userkeyArr = [];
-let userkeyVal = "pgv_pvid=7679460079; ts_last=/activity/page/welwareCenter/; ts_sid=6230379160; ts_uid=8663566700";
-let USERKEY = [];
+let userkeyVal = "";
+let USERKEY = 'pgv_pvid=7679460079; ts_last=/activity/page/welwareCenter/; ts_sid=6230379160; ts_uid=8663566700';
 
 let cashheaderArr = [];
 let cashheaderVal = "";
 let CASHHEADER = [];
 
 let signheaderArr = [];
-let signheaderVal = "oA0GbjhRW4-KztUg8E8SpN7z7kB0&fskey=v0aaf63c220600b82a27e95fb57bb1ff&access_token=41_rlGfPc3HxQeXrVO9Jbv9xTvKftAP6ZZu_Hrohp81i9bCu1IU0MjlI6ieqKQ7yeT_5V94S3Cg0FAppBcBQU4G_v_LDQvUINSFHEbGvHOufJY&_appName=ios&_appver=8.7.1&_osVer=14.3&_devId=8661a562dacc175efc5a8ee552a4dcfa4a31fc53";
-let SINGHEADER = [];
+let signheaderVal = "";
+let SINGHEADER = 'oA0GbjhRW4-KztUg8E8SpN7z7kB0&fskey=v0aaf63c220600b82a27e95fb57bb1ff&access_token=41_rlGfPc3HxQeXrVO9Jbv9xTvKftAP6ZZu_Hrohp81i9bCu1IU0MjlI6ieqKQ7yeT_5V94S3Cg0FAppBcBQU4G_v_LDQvUINSFHEbGvHOufJY&_appName=ios&_appver=8.7.1&_osVer=14.3&_devId=8661a562dacc175efc5a8ee552a4dcfa4a31fc53';
 
 let signkeyArr = [];
-let signkeyVal = "pgv_pvid=7679460079; ts_last=/activity/page/celebratemon/; ts_sid=5670528140; ts_uid=2811203300";
-let SINGKEY = [];
+let signkeyVal = "";
+let SINGKEY = 'pgv_pvid=7679460079; ts_last=/activity/page/celebratemon/; ts_sid=5670528140; ts_uid=2811203300';
 
 let taskheaderArr = [];
-let taskheaderVal = "&_dev=iPhone10,6&_devId=8661a562dacc175efc5a8ee552a4dcfa4a31fc53&_appver=8.7.1&_ifChId=&_isChId=1&_osVer=14.3&openid=oA0GbjhRW4-KztUg8E8SpN7z7kB0&fskey=v0aaf63c220600b82a27e95fb57bb1ff&appid=wxcbc3ab3807acb685&access_token=41_rlGfPc3HxQeXrVO9Jbv9xTvKftAP6ZZu_Hrohp81i9bCu1IU0MjlI6ieqKQ7yeT_5V94S3Cg0FAppBcBQU4G_v_LDQvUINSFHEbGvHOufJY&buildType=store&check=11&_idfa=E67F03D3-A70A-4280-8289-802EE71D155C&lang=zh_CN";
-let TASKHEADER = [];
+let taskheaderVal = "";
+let TASKHEADER = '&_dev=iPhone10,6&_devId=8661a562dacc175efc5a8ee552a4dcfa4a31fc53&_appver=8.7.1&_ifChId=&_isChId=1&_osVer=14.3&openid=oA0GbjhRW4-KztUg8E8SpN7z7kB0&fskey=v0aaf63c220600b82a27e95fb57bb1ff&appid=wxcbc3ab3807acb685&access_token=41_rlGfPc3HxQeXrVO9Jbv9xTvKftAP6ZZu_Hrohp81i9bCu1IU0MjlI6ieqKQ7yeT_5V94S3Cg0FAppBcBQU4G_v_LDQvUINSFHEbGvHOufJY&buildType=store&check=11&_idfa=E67F03D3-A70A-4280-8289-802EE71D155C&lang=zh_CN';
 
 let taskkeyArr = [];
-let taskkeyVal = "pgv_pvid=7679460079; ts_last=/activity/page/welwareCenter/; ts_sid=6230379160; ts_uid=8663566700";
-let TASKKEY = [];
+let taskkeyVal = "";
+let TASKKEY = 'pgv_pvid=7679460079; ts_last=/activity/page/welwareCenter/; ts_sid=6230379160; ts_uid=8663566700';
 
 let wxtaskkeyArr = [];
-let wxtaskkeyVal = "wzq_channel=fm_wzq_wx_v1_unknow_01..; pgv_info=ssid=s1643225638; pgv_pvid=4536938914; qlappid=wx9cf8c670ebd68ce4; qlskey=v0954858a12600b8542e093d467a447b; qluin=085e9858e838948259313c1e0@wx.tenpay.com; qq_logtype=16; wx_session_time=1611367746000; wzq_qlappid=wx9cf8c670ebd68ce4; wzq_qlskey=v0954858a12600b8542e093d467a447b; wzq_qluin=os-ppuJARH-L4uWsmvHWRqiz9C80";
-let WXTASKKEY = [];
+let wxtaskkeyVal = "";
+let WXTASKKEY = 'wzq_channel=fm_wzq_wx_v1_unknow_01..; pgv_info=ssid=s1643225638; pgv_pvid=4536938914; qlappid=wx9cf8c670ebd68ce4; qlskey=v0954858a12600b8542e093d467a447b; qluin=085e9858e838948259313c1e0@wx.tenpay.com; qq_logtype=16; wx_session_time=1611367746000; wzq_qlappid=wx9cf8c670ebd68ce4; wzq_qlskey=v0954858a12600b8542e093d467a447b; wzq_qluin=os-ppuJARH-L4uWsmvHWRqiz9C80';
 
 ////////////////////////////////////////////////////////////////////
 
