0598a6eb7bcd1e3e57217c2106eaca137c7c3e72
lpssxs <731588359@qq.com>
2021-11-27 13:40:40 +0800

    znxq

---
+++ js_code/znxq/znxqCOOKIE.js
@@ -0,0 +1,127 @@
+  
+//boxjs粘贴会话至{}
+module.exports =
+	{
+	"id": "znxq",
+	"name": "臻浓星球",
+	"keys": ["znxqheader", "znxqheader2", "znxqheader3", "znxqheader4", "znxqheader5", "znxqheader6", "znxqheader7", "znxqheader8", "znxqheader9", "znxqheader10", "znxqheader11", "znxqheader12", "znxqheader13", "znxqheader14", "znxqheader15", "znxqheader16", "znxqheader17", "znxqheader18", "znxqheader19", "znxqheader20"],
+	"author": "@ziye",
+	"settings": [{
+		"id": "znxqSuffix",
+		"name": "当前账号",
+		"val": "1",
+		"type": "number",
+		"desc": "当前抓取ck记录的账号序号，如：1、2、3、"
+	}, {
+		"id": "znxqCount",
+		"name": "账号个数",
+		"val": "4",
+		"type": "number",
+		"desc": "指定任务最多跑几个账号，根据抓取的账号数据个数来设值"
+	}, {
+		"id": "znxqXH",
+		"name": "循环获取CK",
+		"val": "0",
+		"type": "number",
+		"desc": "0关闭 1开启,默认关闭"
+	}, {
+		"id": "znxqDH",
+		"name": "兑换商品",
+		"val": "13",
+		"type": "number",
+		"desc": "0关闭 13牛奶,16电饼档,17汤蒸锅,18篮球,19杯子,20筷子,21三件套,22工具套"
+	}, {
+		"id": "znxqXYZ",
+		"name": "执行概率",
+		"val": "100",
+		"type": "number",
+		"desc": "0不执行 可设置0-100,默认百分百"
+	}, {
+		"id": "znxqnotifyttt",
+		"name": "推送控制",
+		"val": "1",
+		"type": "number",
+		"desc": "0关闭，1推送,默认12点以及23点推送"
+	}, {
+		"id": "znxqnotifyInterval",
+		"name": "通知控制",
+		"val": "2",
+		"type": "number",
+		"desc": "0关闭，1为 所有通知，2为 12，23 点通知，3为 6，12，18，23 点通知 "
+	}, {
+		"id": "znxqMinutes",
+		"name": "推送-通知 分钟控制",
+		"val": "10",
+		"type": "number",
+		"desc": "推送以及通知控制在什么分钟段，可设置0-59,默认0到10"
+	}],
+	"repo": "https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/znxq.js",
+	"icons": ["https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/znxq.png", "https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/znxq.png"],
+	"script": "https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/znxq.js",
+	"icon": "https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/znxq.png",
+	"favIcon": "mdi-star-outline",
+	"favIconColor": "grey",
+	"datas": [{
+		"key": "znxqheader",
+		"val": "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0MjI2OTEwIiwiaWF0IjoxNjM3OTEzMTgzLCJzdWIiOiJkc2tqMiIsImlzcyI6ImRza2oxIiwiZXhwIjoxNjQwNTA1MTgzfQ.1s3FWUSKvAixbPyDde1U5QpWZ_3CaKSIUA7gLsns7YY"
+	}, {
+		"key": "znxqheader2",
+		"val": "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0MjI2OTEyIiwiaWF0IjoxNjM3OTEzODUwLCJzdWIiOiJkc2tqMiIsImlzcyI6ImRza2oxIiwiZXhwIjoxNjQwNTA1ODUwfQ.nGCuZx2XQId0C5NvfOd9ijzuM3yGbv15dO_HhM2KEas"
+	}, {
+		"key": "znxqheader3",
+		"val": "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0MjI3NTkzIiwiaWF0IjoxNjM3OTkxMjE5LCJzdWIiOiJkc2tqMiIsImlzcyI6ImRza2oxIiwiZXhwIjoxNjQwNTgzMjE5fQ.sCdr93jvtlcn0FZoXdrnBDKjhKE8YBaUB3EsYKFxizo"
+	}, {
+		"key": "znxqheader4",
+		"val": "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0MjI3NTkyIiwiaWF0IjoxNjM3OTkxMDIxLCJzdWIiOiJkc2tqMiIsImlzcyI6ImRza2oxIiwiZXhwIjoxNjQwNTgzMDIxfQ.QzV80b64oxhhAD_GQmB2eh7vyYqKRGhIT-cNvRQtaUc"
+	}, {
+		"key": "znxqheader5",
+		"val": ""
+	}, {
+		"key": "znxqheader6",
+		"val": ""
+	}, {
+		"key": "znxqheader7",
+		"val": ""
+	}, {
+		"key": "znxqheader8",
+		"val": ""
+	}, {
+		"key": "znxqheader9",
+		"val": ""
+	}, {
+		"key": "znxqheader10",
+		"val": ""
+	}, {
+		"key": "znxqheader11",
+		"val": ""
+	}, {
+		"key": "znxqheader12",
+		"val": ""
+	}, {
+		"key": "znxqheader13",
+		"val": ""
+	}, {
+		"key": "znxqheader14",
+		"val": ""
+	}, {
+		"key": "znxqheader15",
+		"val": ""
+	}, {
+		"key": "znxqheader16",
+		"val": ""
+	}, {
+		"key": "znxqheader17",
+		"val": ""
+	}, {
+		"key": "znxqheader18",
+		"val": ""
+	}, {
+		"key": "znxqheader19",
+		"val": ""
+	}, {
+		"key": "znxqheader20",
+		"val": ""
+	}],
+	"sessions": [],
+	"isLoaded": true
+}
